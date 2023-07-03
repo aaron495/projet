036b1228ca709238e1ee6981e13530ffae89a097
aaron495 <121938441+aaron495@users.noreply.github.com>
2023-07-03 07:25:14 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -16,3 +16,26 @@ menuItems.forEach(function(item) {
   });
 });
 
+// Fonction pour gérer l'événement de soumission du formulaire
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêche l'envoi du formulaire
+
+  // Récupère les valeurs des champs du formulaire
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Faites quelque chose avec les valeurs (envoi du formulaire, etc.)
+  console.log('Nom : ' + name);
+  console.log('Email : ' + email);
+  console.log('Message : ' + message);
+
+  // Réinitialise le formulaire
+  document.getElementById('contactForm').reset();
+});
+
+
+
+
+
+
